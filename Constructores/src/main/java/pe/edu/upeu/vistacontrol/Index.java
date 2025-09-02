c30495b726383846a171b3bb705ecc175a0aa3e7
Pedro Eduardo Loayza huaman <pfloayza1704@gmail.com>
2025-09-02 16:00:58 -0500

    added

---
+++ Constructores/src/main/java/pe/edu/upeu/vistacontrol/Index.java
@@ -0,0 +1,21 @@
+package pe.edu.upeu.vistacontrol;
+
+import pe.edu.upeu.modelo.Persona;
+
+/**
+ * @author Daniel Levano
+ */
+public class Index {
+    public static void inicio(){
+        Persona persona = new Persona("Pablito", 16, 1.73);
+
+        // Mostrar los valores del Objeto
+        System.out.println("DATOS INGRESADOS");
+        System.out.println("* Nombre: " + persona.getNombre());
+        System.out.println("* Edad: " + persona.getEdad());
+        System.out.println("* Talla: " + persona.getTalla());
+    }
+    public static void main(String[] args) {
+        inicio();
+    }
+}
