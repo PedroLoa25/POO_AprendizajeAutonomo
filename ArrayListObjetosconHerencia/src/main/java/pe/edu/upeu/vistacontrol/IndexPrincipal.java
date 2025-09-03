dff109e5318cce5b76e6db520dc175a61633d37b
Pedro Eduardo Loayza huaman <pfloayza1704@gmail.com>
2025-09-02 21:51:18 -0500

    added

---
+++ ArrayListObjetosconHerencia/src/main/java/pe/edu/upeu/vistacontrol/IndexPrincipal.java
@@ -0,0 +1,10 @@
+package pe.edu.upeu.vistacontrol;
+
+/**
+ * @author Daniel Lévano
+ */
+public class IndexPrincipal {
+    public static void inicio(){
+        IndexTrabajador.inicio();
+    }
+}
